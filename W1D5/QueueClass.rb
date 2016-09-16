8c992944b961d1c6897229db7044d9726e534cd1
Amanda Fielding <amandafielding@Amandas-MacBook-Pro.local>
2016-09-15 21:21:08 -0700

    completed DIYADTs homework

---
+++ W1D5/QueueClass.rb
@@ -0,0 +1,17 @@
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue. << el
+  end
+
+  def dequeue(el)
+    @queue.shift
+  end
+
+  def show
+    @queue.dup
+  end
+end
